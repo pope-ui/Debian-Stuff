155a362cf71436b3c97e784115f5bdef35e1d782
pope-ui <peter.cherdron@gmx.de>
2024-08-16 10:37:31 +0200

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -2,12 +2,21 @@
 cd ~/Downloads
 git Clone https://github.com/drewgrif/dotfiles
 
-\cp -r ~/Downloads/dotfiles/bin/ ~
+cp -r ~/Downloads/dotfiles/bin/ ~
 chmod +x ~/bin/*
 sudo cp ~~/bin/pulseaudio-control.bash /bin/
-\cp -r ~/Downloads/dotfiles/.config/dunst/ ~/.config/
-\cp -r ~/Downloads/dotfiles/.config/picom ~/.config/
-\cp -r ~/Downloads/dotfiles/.config/polybar ~/.config/
-\cp -r ~/Downloads/dotfiles/.config/rofi ~/.config/
-\cp -r ~/Downloads/dotfiles/.config/Backgrounds ~/.config/
-\cp -r ~/Downloads/dotfiles/.config/sxhkd ~/.config/
+\cp -r ~/Downloads/jag_dots/.config/dunst/ ~/.config/
+\cp -r ~/Downloads/jag_dots/.config/picom ~/.config/
+\cp -r ~/Downloads/jag_dots/.config/polybar ~/.config/
+\cp -r ~/Downloads/jag_dots/.config/rofi ~/.config/
+\cp -r ~/Downloads/jag_dots/.config/Backgrounds ~/.config/
+\cp -r ~/Downloads/jag_dots/.config/fastfetch ~/.config/
+\cp -r ~/Downloads/jag_dots/.config/micro ~/.config/
+\cp -r ~/Downloads/jag_dots/.config/kitty ~/.config/
+\cp -r ~/Downloads/jag_dots/.config/geany/colorschemes ~/.config/
+\cp -r ~/Downloads/jag_dots/.config/gtk3 ~/.config/
+\cp -r ~/Downloads/jag_dots/.config/nvim ~/.config/
+\cp -r ~/Downloads/jag_dots/.config/tint ~/.config/
+\cp -r ~/Downloads/jag_dots/.config/waybar ~/.config/
+\cp -r ~/Downloads/jag_dots/.config/sway ~/.config/
+\cp -r ~/Downloads/jag_dots/.config/wofi ~/.config/
